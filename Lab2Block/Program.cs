b49510c8443568ed454769cfee496b74be033751
kalos <kalos@DESKTOP-L59HKEO>
2024-11-09 08:39:16 +0200

    Добавьте файлы проекта.

---
+++ Lab2Block/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2Block
+{
+    class Program
+    {
+        private string name;
+        private int surname;
+        private System.DateTime dataofbirth;
+
+
+
+
+
+
+
+
+        static void Main(string[] args)
+        {
+
+
+
+        }
+    }
+}
